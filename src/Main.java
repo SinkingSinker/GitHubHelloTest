85365012b39cc144a9e4ce23510281876ce12613
HansErritzoe <h.erritzoe@hotmail.com>
2024-04-03 09:26:13 +0200

    Push 3 test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("test2");
         System.out.println("Push 1");
+        System.out.println("push 3");
     }
 }
